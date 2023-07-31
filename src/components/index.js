74664af2ac105d8b0125ad0c831e75dc034df122
Raghav Aggarwal <aggarwalchirag2000@gmail.com>
2023-07-31 22:57:31 +0530

    blog list

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,7 @@
 import Navbar from "./Navbar";
+import BlogList from "./BlogList";
 
 export{
-    Navbar
+    Navbar,
+    BlogList
 }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,19 +1,24 @@
 import { useState } from "react";
+import { BlogList } from "../../components";
 
 const Home = () => {
     const [blogs, setBlogs] = useState([
         { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
         { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-      ])
+      ]);
+
+      const handleDelete = (id) =>{
+        const newblogs = blogs.filter(blog => blog.id !== id);
+        setBlogs(newblogs);
+      }
     return ( 
         <div className="home">
-            {blogs.map(blog =>(
-                <div className="blog-preview" key={blog.id} >
-                <h2>{ blog.title }</h2>
-                <p>Written by { blog.author }</p>
-                </div> 
-            ))}
+            <BlogList 
+            blogs = {blogs}
+            title = "All Blogs"
+            handleDelete = {handleDelete}
+            />
         </div>
      );
 }
